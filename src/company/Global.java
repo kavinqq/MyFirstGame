a0b3b12104021b4d0e9a3ba0e62bd742282a3624
DESKTOP-0FM3OLF\cc780 <cc780406@gmail.com>
2022-03-18 19:43:06 +0800

    feat 主畫面架構

--- src/company/Global.java
+++ src/company/Global.java
@@ -2,6 +2,8 @@ package company;
 
 public class Global {
 
+
+
     public enum Direction {
         UP,
         DOWN,
@@ -17,11 +19,47 @@ public class Global {
     public static final int FRAME_LIMIT = 60;
     public static final int LIMIT_DELTA_TIME = 1000000000 / FRAME_LIMIT;
 
-    //視窗大小
+    //整個視窗大小
     public static final int WINDOW_WIDTH = 1920;
     public static final int WINDOW_HEIGHT = 1080;
+
+    //狀態攔
+    public static final int STATUS_BAR_X=0;
+    public static final int STATUS_BAR_Y=0;
+    public static final int STATUS_BAR_WEIGHT = WINDOW_WIDTH;
+    public static final int STATUS_BAR_HEIGHT=60;
+
+    //遊戲視窗大小
     public static final int SCREEN_X = WINDOW_WIDTH - 8 - 8;
     public static final int SCREEN_Y = WINDOW_HEIGHT - 31 - 8;
+
+    //選單大小
+    public static final int BUILDING_OPTION_WIDTH=240;
+    public static final int BUILDING_OPTION_HEIGHT=SCREEN_Y;
+    public static final int BUILDING_OPTION_X=WINDOW_WIDTH-BUILDING_OPTION_WIDTH;
+    public static final int BUILDING_OPTION_Y=0;
+
+
+    //領地大小
+    public static final int LAND_WIDTH=1320;
+    public static final int LAND_HEIGHT=840;
+    public static final int LAND_X=(SCREEN_X-LAND_WIDTH-BUILDING_OPTION_WIDTH)/2;
+    public static final int LAND_Y=SCREEN_Y/2-LAND_HEIGHT/2;
+
+    //建築物大小
+    public static final int BUILDING_WIDTH=110;
+    public static final int BUILDING_HEIGHT=130;
+
+    //基座大小
+    public static final int FOUNDATION_WIDTH=120;
+    public static final int FOUNDATION_HEIGHT=120;
+    public static final int FOUNDATION_DISTANCE_X=120;
+    public static final int FOUNDATION_DISTANCE_Y=120;
+
+
+    //建築
+    public final static int BUILDING_AMOUNT_X=LAND_WIDTH/FOUNDATION_WIDTH/2;
+    public final static int BUILDING_AMOUNT_Y=LAND_HEIGHT/FOUNDATION_HEIGHT/2;
 
     /**
      * 操作指令
--- src/company/scene/MainScene.java
+++ src/company/scene/MainScene.java
@@ -13,6 +13,8 @@ import company.gametest9th.utils.Path;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
+import static company.Global.*;
+
 /**
  * 遊戲主場景
  */
@@ -21,16 +23,28 @@ public class MainScene extends Scene {
     private Image img;
 
     private RockFactory rockFactory;
-    private Foundation foundation;
+    private Foundation[][] foundation=new Foundation[BUILDING_AMOUNT_X][BUILDING_AMOUNT_Y];
     private Road road;
+
+
+
 
     @Override
     public void sceneBegin() {
         img = SceneController.getInstance().imageController().tryGetImage(new Path().img().background().background());
+        for (int i = 0; i < BUILDING_AMOUNT_X; i++) {
+//            if(i==2){
+//                i++;
+//                continue;
+//            }
+            for (int j = 0; j < BUILDING_AMOUNT_Y; j++) {
+                foundation[i][j] = new Foundation(FOUNDATION_WIDTH+ LAND_X+i* FOUNDATION_WIDTH*2,
+                        FOUNDATION_HEIGHT+ LAND_Y
+                        +j* FOUNDATION_HEIGHT*2, FOUNDATION_WIDTH, FOUNDATION_HEIGHT);
+            }
 
-        rockFactory = new RockFactory(370,220,110,130);
-
-        foundation = new Foundation(370,220,120,120);
+        }
+        rockFactory = new RockFactory(LAND_X, LAND_Y, BUILDING_WIDTH, BUILDING_HEIGHT);
 
 //        road = new Road()
     }
@@ -41,15 +55,24 @@ public class MainScene extends Scene {
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(img, 0, 0, Global.SCREEN_X, Global.SCREEN_Y, null);
+        //背景
+        g.drawImage(img, 0, 0, SCREEN_X, SCREEN_Y, null);
 
-        foundation.paint(g);
+        //狀態攔
+        g.drawRect(STATUS_BAR_X, STATUS_BAR_Y, STATUS_BAR_WEIGHT, STATUS_BAR_HEIGHT);
+        for (int i = 0; i < BUILDING_AMOUNT_X; i++) {
+            for (int j = 0; j < BUILDING_AMOUNT_Y; j++) {
+                foundation[i][j].paint(g);
+            }
+        }
+
+        //建築物選單
+        g.drawRect(BUILDING_OPTION_X, BUILDING_OPTION_Y,BUILDING_OPTION_WIDTH,BUILDING_OPTION_HEIGHT);
+
         rockFactory.paint(g);
 
-
-
         g.setColor(Color.black);
-        g.drawRect(Global.SCREEN_X / 2 - 650, Global.SCREEN_Y / 2 - 350, 1300,700);
+        g.drawRect(LAND_X, LAND_Y, LAND_WIDTH, LAND_HEIGHT);
 
 
     }
@@ -57,7 +80,7 @@ public class MainScene extends Scene {
     @Override
     public void update() {
     }
-    private boolean isCatchrockFactory=false;
+    private boolean isCatchrockFactory;
     @Override
     public CommandSolver.MouseCommandListener mouseListener() {
         return (e, state, trigTime) -> {
