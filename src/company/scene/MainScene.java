e7018ee6d8af5e5568103003457e90cf1026acb3
DESKTOP-0FM3OLF\cc780 <cc780406@gmail.com>
2022-04-07 14:22:34 +0800

    feat 結束畫面

--- src/company/scene/MainScene.java
+++ src/company/scene/MainScene.java
@@ -175,6 +175,7 @@ public class MainScene extends Scene implements CommandSolver.KeyListener {
 
     @Override
     public void sceneEnd() {
+
     }
 
     @Override
@@ -998,12 +999,20 @@ public class MainScene extends Scene implements CommandSolver.KeyListener {
             city.doCityWorkAndTimePass(thisRoundTimePass);
         }
         //20分鐘後結束 或主堡死去 或巫妖王死去
-        if (!city.isAlive() ) { // 時間到遊戲結束|| StatusBar.instance().getTime()>20*60
-            StartScene startScene = new StartScene(); //還沒有結束畫面已此充當結束遊戲
-//            SceneController.getInstance().change(startScene);
-        }
+        if (!city.isAlive()|| StatusBar.instance().getTime()>20*60) {//
 
+            EndScene endScene = new EndScene(); //還沒有結束畫面已此充當結束遊戲
+            //如果城市活者
+            if (city.isAlive()) {
+                endScene.setWin(true);
+            }else{
+                endScene.setWin(false);
+            }
+//            SceneController.getInstance().change(endScene);
+        }
     }
+
+
 
 
     @Override
